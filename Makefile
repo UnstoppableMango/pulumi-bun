5cf76ccf0936867a013169a108610e1ef3fa8571
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-10-19 16:37:55 -0500

    Pulumi provides most of the generated gRPC stubs...

--- Makefile
+++ Makefile
@@ -5,26 +5,18 @@ PROJECT     := github.com/unstoppablemango/pulumi-bun
 GO_SRC := $(shell find . -type f -name '*.go')
 
 LOCALBIN := ${WORKING_DIR}/bin
-BUF      := ${LOCALBIN}/buf
 PULUMI   := ${LOCALBIN}/pulumi
 
 build: bin/pulumi-language-bun
 
 tools: $(addprefix bin/,buf pulumi)
-proto: pulumi-language-bun/proto
 tidy: pulumi-language-bun/go.sum
 
 clean:
-	rm -rf pulumi-language-bun/proto
+	rm -rf bin/pulumi-language-bun
 
-pulumi-language-bun/proto: | $(BUF)
-	$(BUF) generate
-
-bin/pulumi-language-bun: $(filter ./pulumi-language-bun/%,$(GO_SRC))
+bin/pulumi-language-bun: $(filter ./pulumi-language-bun/%,$(GO_SRC)) pulumi-language-bun/go.sum
 	go -C pulumi-language-bun build -o ${WORKING_DIR}/$@ ./
-
-bin/buf: .versions/buf
-	GOBIN=${LOCALBIN} go install github.com/bufbuild/buf/cmd/buf@v$(shell cat $<)
 
 bin/pulumi: .versions/pulumi
 	curl -fsSL https://get.pulumi.com | sh -s -- --install-root ${WORKING_DIR} --version $(shell cat $<) --no-edit-path
--- pulumi-language-bun/language.go
+++ pulumi-language-bun/language.go
@@ -1,71 +1,24 @@
 package main
 
 import (
-	"context"
-
-	"connectrpc.com/connect"
-	"github.com/unstoppablemango/pulumi-bun/pulumi-language-bun/proto/pulumi"
-	"google.golang.org/protobuf/types/known/emptypb"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
 )
 
-type LanguageServer struct{}
+type bunLanguageHost struct {
+	pulumirpc.UnimplementedLanguageRuntimeServer
 
-// About implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) About(context.Context, *connect.Request[pulumi.AboutRequest]) (*connect.Response[pulumi.AboutResponse], error) {
-	panic("unimplemented")
+	exec                 string
+	engineAddress        string
+	tracing              string
+	binary               string
+	dotnetBuildSucceeded bool
 }
 
-// GeneratePackage implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) GeneratePackage(context.Context, *connect.Request[pulumi.GeneratePackageRequest]) (*connect.Response[pulumi.GeneratePackageResponse], error) {
-	panic("unimplemented")
-}
-
-// GenerateProgram implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) GenerateProgram(context.Context, *connect.Request[pulumi.GenerateProgramRequest]) (*connect.Response[pulumi.GenerateProgramResponse], error) {
-	panic("unimplemented")
-}
-
-// GenerateProject implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) GenerateProject(context.Context, *connect.Request[pulumi.GenerateProjectRequest]) (*connect.Response[pulumi.GenerateProjectResponse], error) {
-	panic("unimplemented")
-}
-
-// GetPluginInfo implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) GetPluginInfo(context.Context, *connect.Request[emptypb.Empty]) (*connect.Response[pulumi.PluginInfo], error) {
-	panic("unimplemented")
-}
-
-// GetProgramDependencies implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) GetProgramDependencies(context.Context, *connect.Request[pulumi.GetProgramDependenciesRequest]) (*connect.Response[pulumi.GetProgramDependenciesResponse], error) {
-	panic("unimplemented")
-}
-
-// GetRequiredPlugins implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) GetRequiredPlugins(context.Context, *connect.Request[pulumi.GetRequiredPluginsRequest]) (*connect.Response[pulumi.GetRequiredPluginsResponse], error) {
-	panic("unimplemented")
-}
-
-// InstallDependencies implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) InstallDependencies(context.Context, *connect.Request[pulumi.InstallDependenciesRequest], *connect.ServerStream[pulumi.InstallDependenciesResponse]) error {
-	panic("unimplemented")
-}
-
-// Pack implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) Pack(context.Context, *connect.Request[pulumi.PackRequest]) (*connect.Response[pulumi.PackResponse], error) {
-	panic("unimplemented")
-}
-
-// Run implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) Run(context.Context, *connect.Request[pulumi.RunRequest]) (*connect.Response[pulumi.RunResponse], error) {
-	panic("unimplemented")
-}
-
-// RunPlugin implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) RunPlugin(context.Context, *connect.Request[pulumi.RunPluginRequest], *connect.ServerStream[pulumi.RunPluginResponse]) error {
-	panic("unimplemented")
-}
-
-// RuntimeOptionsPrompts implements pulumiconnect.LanguageRuntimeHandler.
-func (l *LanguageServer) RuntimeOptionsPrompts(context.Context, *connect.Request[pulumi.RuntimeOptionsRequest]) (*connect.Response[pulumi.RuntimeOptionsResponse], error) {
-	panic("unimplemented")
+func newLanguageHost(exec, engineAddress, tracing string, binary string) pulumirpc.LanguageRuntimeServer {
+	return &bunLanguageHost{
+		exec:          exec,
+		engineAddress: engineAddress,
+		tracing:       tracing,
+		binary:        binary,
+	}
 }
